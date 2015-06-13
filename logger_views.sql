96c13faa81b1a6aaa873dcab0776b2518579c635
Gleidson Echeli <el.gleidson@gmail.com>
2015-06-13 10:10:15 -0300

    Fatal views

--- logger_views.sql
+++ logger_views.sql
@@ -36,3 +36,24 @@ from logger_logs
 where log_date >= (sysdate - (1 / 24))
 and log_level = 'ERROR';
 /
+
+create or replace view logger_fatal_today as
+select * 
+from logger_logs
+where log_date >= trunc(sysdate)
+and log_level = 'FATAL';
+/
+
+create or replace view logger_fatal_24h as
+select * 
+from logger_logs
+where log_date >= (sysdate-1)
+and log_level = 'FATAL';
+/
+
+create or replace view logger_fatal_1h as
+select * 
+from logger_logs
+where log_date >= (sysdate - (1 / 24))
+and log_level = 'FATAL';
+/
